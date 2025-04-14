06da57f88fd85c11c102e4fe2b4bfd61d78362e5
SY1447 <your@email.com>
2025-04-14 22:31:10 +0900

    Fix: Remove npm build commands, update build process

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1744565631',resources: [
+    cacheName: 'chirpy-1744637443',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
